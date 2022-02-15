28b77f061a18b8e9ca9a1dcf856a9fda1e5ed195
Knut Waale <kwaale@github.com>
2022-02-15 14:40:48 -0300

    prueba

--- src/Adapters/index.js
+++ src/Adapters/index.js
@@ -1,12 +1,12 @@
 const { Services } = require('../Services');
 
-async function Adapter({id}){
+async function Adapter({ id }) {
     try {
-        let { statusCode, data, message } = await Services({id});
-        return {statusCode, data, message}
+        let { statusCode, data, message } = await Services({ id });
+        return { statusCode, data, message }
     } catch (error) {
-        console.error({step: 'adapter Adapter', error: error.toString()});
-        return {statusCode:500, message: error.toString()};
+        console.error({ step: 'adapter Adapter', error: error.toString() });
+        return { statusCode: 500, message: error.toString() };
     }
 }
 module.exports = {
